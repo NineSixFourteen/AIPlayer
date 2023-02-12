e82e21e43d58ec7fec35151c547d01b2ec9d22d4
NineSixFourteen <thomas-williams-1@outlook.com>
2023-02-12 00:59:23 +0000

    added websocket

--- API/maincontroller.go
+++ API/maincontroller.go
@@ -1,23 +1,26 @@
 package API
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10/translations/id"
+	"github.com/gorilla/websocket"
 )
+var upgrader = websocket.Upgrade{};
 
 type Connection struct {
-  ID int32 
-  // Websocket connection 
+  ID int64 
+  Socket websocket  
   // Game game  
 }
 
 type ReceiveMessage struct {
   State string 
-  Turn  int32
+  Turn  int64
   isComplete bool 
-  Winner int32
+  Winner int64
 }
 
 type SendMessage struct {
@@ -27,11 +30,48 @@ type SendMessage struct {
 }
 
 var connections = []Connection {
-  {ID:0}
+  {ID:0},
 }
+
+func addConnection(c *gin.Context){
+    id := c.Param("id")
+    var newCon Connection; 
+    newCon.ID, _ = strconv.ParseInt(id,10,32)
+    check := true;
+    for _, con := range connections {
+          if(con.ID == newCon.ID){
+              check = false;
+              break;
+          }
+    }
+    if(check){
+      connections = append(connections, newCon)
+    }
+
+}
+
+func createConnection(w http.ResponseWriter, r *http.Request){
+  conn, err := upgrader.Upgrad(w,r, nil) 
+  if err != nil {
+    fmt.Println("Failed to create connection")
+    return
+  }
+
+  for {
+  t, msg, err := conn.ReadMessage()
+  if err != nil {
+    break
+  }
+  conn.WriteMessage(t, msg) 
+  }
+}
+
 
 func main(){
   router := gin.Default()
-
+  router.GET("newCon:id", addConnection)
+  router.GET("/ws", func(c *gin.Context) {
+    createConnection(c.Writer, c.Request)
+  })
   router.Run("localhost:8080")
 }
